db7ebf0cfcdd96cbdb8fec87ff72ad40dfcd0375
roshanrabadiya <39290178+roshanrabadiya@users.noreply.github.com>
2019-10-04 19:12:19 +0530

    Update 1.2 function_and_class.kt

--- 1.2 function_and_class.kt
+++ 1.2 function_and_class.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>){
 
-    var myObj = MyFirstClass()  // This is the Object of the MyFirst Class. Hear yor can't use "new" Keyword
+    var myObj = MyFirstClass()  // This is the Object of the MyFirst Class. Hear you can't use "new" Keyword
     myObj.display()    // calling the display Function
     myObj.add(14,6)  // calling the add Function
 }
