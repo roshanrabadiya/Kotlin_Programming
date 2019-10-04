19d0093fa5207e9b9d861cefc4cbbb0c8b2738a9
roshanrabadiya <39290178+roshanrabadiya@users.noreply.github.com>
2019-10-04 19:11:38 +0530

    Function & Class

---
+++ 1.2 function_and_class.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>){
+
+    var myObj = MyFirstClass()  // This is the Object of the MyFirst Class. Hear yor can't use "new" Keyword
+    myObj.display()    // calling the display Function
+    myObj.add(14,6)  // calling the add Function
+}
+
+class MyFirstClass{
+
+    // Function
+    fun display(){
+        var myName:String
+        myName = "Google"
+        println(myName)
+    }
+
+    // Function
+    fun add(a: Int,b: Int){
+        var sum = a + b;
+        println("Sum is $sum");
+    }
+}
