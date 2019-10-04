d57c041e9dcd3797d356799ccecddcad565a2ba5
roshanrabadiya <39290178+roshanrabadiya@users.noreply.github.com>
2019-10-04 19:32:12 +0530

    Create Class

---
+++ Class/MyFirstClass.kt
@@ -0,0 +1,16 @@
+package com.roshan
+
+class MyFirstClass {
+    // Function
+    fun display(){
+        var myName:String
+        myName = "Google"
+        println(myName)
+    }
+
+    // Function
+    fun add(a: Int,b: Int){
+        var sum = a + b;
+        println("Sum is $sum");
+    }
+}
