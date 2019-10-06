3ee41fd972937fedc7f1490948b3f20d895b44ba
roshanrabadiya <39290178+roshanrabadiya@users.noreply.github.com>
2019-10-06 13:00:05 +0530

    Functions or Methods

---
+++ 5.1 function_or_method.kt
@@ -0,0 +1,19 @@
+// Objective
+
+// 1. Methods or Function in Kotlin
+
+fun main(){
+   addNumbers()         // hear i simply call the assNumbers Method or Function
+   val multiplication = mul(3,5)
+    println("multiplication is $multiplication")
+}
+
+fun addNumbers(): Unit{  // in Kotlin 'void' is replace with 'Unit' keyword. but you skip the 'Unite' keyword if function is nothing return
+    val a: Int = 10
+    val b: Int = 12
+    println("Sum is ${a+b}")
+}
+
+fun mul(a: Int, b: Int): Int{
+    return a*b
+}
